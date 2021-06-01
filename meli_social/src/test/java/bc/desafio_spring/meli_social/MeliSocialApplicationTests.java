557321a3b745be4d7c24b05cf17a823ecfef30cd
TonPaes <ton.paes.bccs@gmail.com>
2021-06-01 11:57:14 -0300

    first commit :D

---
+++ meli_social/src/test/java/bc/desafio_spring/meli_social/MeliSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package bc.desafio_spring.meli_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
